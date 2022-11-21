c856d90e147f424ca6b40db7df4458291414b961
LYRbbding <35730466+LYRbbding@users.noreply.github.com>
2022-11-21 22:38:04 +0800

    fix: dist dir

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.jsdelivr.net/gh/LYRbbding/nuclear-ts-frontend@1.0.0-221121/dist' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.jsdelivr.net/gh/LYRbbding/nuclear-ts-frontend@1.0.1-221121/dist' : '/',
   pages: {
     index: {
       entry: 'src/main.ts',
